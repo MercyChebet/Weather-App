e731d4dcb0aa72784afbc847d9b631ec5d7c0c7b
MercyChebet <chebetm96@gmail.com>
2022-09-01 10:16:34 +0300

    defined endpoints urls

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const base_url = 'https://api.openweathermap.org/data/2.5/'
+const weather_url = base_url + 'weather?q='
+const forecast_url = base_url + 'forecast?q='
+const api_key = '&APPID=1729a6b082fe1edb7990e2bea663bb38'
+
+
