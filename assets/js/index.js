6480be8d9365e1190789c618881708a701057844
MercyChebet <chebetm96@gmail.com>
2022-09-01 19:20:05 +0300

    set loading in cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,23 @@ const forecast_url = base_url + 'forecast?q='
 const api_key = '&APPID=1729a6b082fe1edb7990e2bea663bb38'
 
 document.addEventListener('DOMContentLoaded', () => {
-
+      let card1 = document.createElement('div')
+      let card2 = document.createElement('div')
+      let card3 = document.createElement('div')
+      card1.classList.add('card-day')
+      card1.classList.add('card-day')
+      card2.classList.add('card-day')
+      setLoading(card1)
+      setLoading(card2)
+      setLoading(card3)
       fetchData('weather', 'London').then(data => {
-            renderData(data)
+            renderData(data, card1)
+      })
+      fetchData('weather', 'Nairobu').then(data => {
+            renderData(data, card2)
+      })
+      fetchData('weather', 'New York').then(data => {
+            renderData(data, card3)
       })
 
 })
@@ -17,13 +31,11 @@ async function fetchData(type, city) {
       return await response.json()
 }
 
-function renderData(data) {
-      // console.log(data)
+function renderData(data, card) {
       const hours = new Date().getHours()
       const isDayTime = hours > 6 && hours < 20;
       const time = isDayTime ? 'Day' : 'Night'
       let parent = document.querySelector('.display')
-      let card = document.createElement('div')
       let classNm = 'card-' + time.toLocaleLowerCase()
       card.className = classNm
       let html = ''
@@ -53,16 +65,35 @@ function renderData(data) {
             console.log('forecast')
             fetchData('forecast', data.name).then(data => {
                   console.log(data)
-                  renderForecast(data,card)
+                  renderForecast(data, card)
             })
       })
+      removeLoading(card)
       card.innerHTML = html
       card.appendChild(forecastBtn)
       parent.appendChild(card)
 
 }
 
-function renderForecast(data,card) {
+function setLoading(card) {
+      let html = `
+      <div class="loading">
+            Loading...
+      </div>
+      `
+      card.innerHTML = html
+
+}
+
+function removeLoading(card) {
+      card.innerHTML = ""
+}
+
+function renderForecast(data, card) {
+      let description = data.list[0].weather[0].description
+      console.log(description)
+      console.log(data.list[0])
+      card.innerHTML = description
       // console.log(data.list[0].weather[0].description)
       // console.log(card)
       // let html = ''
